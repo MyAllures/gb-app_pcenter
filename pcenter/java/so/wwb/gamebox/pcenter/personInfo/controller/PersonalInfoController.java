38c20fe8b296a53ac5caab70d6dcce49410830c4
steffan <steffan@dawoo.com>
2018-08-23 18:01:16 +0800

    短信防刷:IP

--- pcenter/java/so/wwb/gamebox/pcenter/personInfo/controller/PersonalInfoController.java
+++ pcenter/java/so/wwb/gamebox/pcenter/personInfo/controller/PersonalInfoController.java
@@ -40,6 +40,7 @@ import so.wwb.gamebox.common.dubbo.ServiceSiteTool;
 import so.wwb.gamebox.common.dubbo.ServiceTool;
 import so.wwb.gamebox.model.*;
 import so.wwb.gamebox.model.common.Audit;
+import so.wwb.gamebox.model.common.RegExpConstants;
 import so.wwb.gamebox.model.common.notice.enums.ContactWayType;
 import so.wwb.gamebox.model.company.enums.SiteOperateStatusEnum;
 import so.wwb.gamebox.model.company.site.po.SiteOperateArea;
@@ -65,6 +66,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.io.Serializable;
 import java.util.*;
+import java.util.regex.Pattern;
 
 /**
  * Created by eagle on 15-10-29.
@@ -608,7 +610,7 @@ public class PersonalInfoController {
     public Map getPhoneVerificationCode(String phone, String verificationCodeToken, HttpServletRequest request) {
 
         Map map = new HashMap();
-        if (phone == null || "".equals(phone)) {
+        if (phone == null || "".equals(phone) || !Pattern.matches(RegExpConstants.CELL_PHONE, phone)) {
             map.put("state", false);
             map.put("msg", LocaleTool.tranMessage(Module.MASTER_SETTING, "personal.phone.notBlank"));
             return map;
@@ -625,11 +627,11 @@ public class PersonalInfoController {
             return map;
         }
 
-        //防刷，IP+USER-AGENT
-//        if (!VerificationCodeTool.validVerificationCodeToken(verificationCodeToken, request)) {
-//            map.put("state", false);
-//            return map;
-//        }
+        //IP防刷，同一个IP90s内有重复，就不发送短信
+        if (VerificationCodeTool.validIp(request)) {
+            map.put("state", false);
+            return map;
+        }
 
         boolean success = MessageSendTool.sendMessage(phone, request);
 
